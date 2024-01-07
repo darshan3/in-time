a10ad9cca418c31f50c284a3187c735bba077f8e
Darshan Tank <darshantank3@gmail.com>
2024-01-08 00:00:17 +0530

    Display start and end time

--- popup/index.js
+++ popup/index.js
@@ -1,18 +1,25 @@
 let timer;
 let startTime;
+let startFormattedTime;
+let stopFormattedTime;
 
 function startTimer() {
   startTime = new Date().getTime();
+  startFormattedTime = getFormattedTime();
   timer = setInterval(updateTimer, 1000);
 }
 
 function stopTimer() {
   clearInterval(timer);
+  stopFormattedTime = getFormattedTime();
+  updateDisplay();
 }
 
 function resetTimer() {
   clearInterval(timer);
-  document.getElementById("timerDisplay").textContent = "00:00:00";
+  startFormattedTime = null;
+  stopFormattedTime = null;
+  updateDisplay();
 }
 
 function updateTimer() {
@@ -23,7 +30,21 @@ function updateTimer() {
   const hours = Math.floor(minutes / 60);
 
   const formattedTime = `${pad(hours)}:${pad(minutes % 60)}:${pad(seconds % 60)}`;
-  document.getElementById("timerDisplay").textContent = formattedTime;
+  updateDisplay(formattedTime);
+}
+
+function updateDisplay(currentTime = "00:00:00") {
+  document.getElementById("timerDisplay").textContent = currentTime;
+  document.getElementById("startTime").textContent = startFormattedTime;
+  document.getElementById("stopTime").textContent = stopFormattedTime;
+}
+
+function getFormattedTime() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
 }
 
 function pad(number) {
